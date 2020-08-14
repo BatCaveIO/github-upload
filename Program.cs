d80f226e4cce159b9212d2bafd160aec4e005435
BatMAN <BatMAN@BatCaveDev.local>
2020-08-14 19:58:58 -0400

    initializing repository

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace iConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Clear();
+            Console.WriteLine("Hello Tristan!!!");
+            Console.WriteLine("");
+            Console.WriteLine("The Time is " + DateTime.Now);
+        }
+    }
+}
